305cda7c20ed1c4878c0c99eaa9fa6b8a50e8f4e
Wen YE <whusnoopy@gmail.com>
2019-02-19 13:43:16 +0800

    ADD: update ddns on dnspod.cn

---
+++ tools/update_ddns.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script used for update local ip to ddns on dnspod.cn
+
+# get token from https://www.dnspod.cn/console/user/security follow https://support.dnspod.cn/Kb/showarticle/tsid/227/
+TOKEN_ID="TODO"
+TOKEN="TODO"
+DOMAIN="TODO"
+SUB_DOMAIN="TODO"
+
+REQUEST_PARAM="login_token=${TOKEN_ID},${TOKEN}&format=json&domain=${DOMAIN}&sub_domain=${SUB_DOMAIN}"
+
+record_info=$(curl -s -X POST https://dnsapi.cn/Record.List -d "${REQUEST_PARAM}")
+record_id=$(echo ${record_info} | grep -Po "(?<=\[\{\"id\":\")([0-9\.]*)")
+record_value=$(echo ${record_info} | grep -Po "(?<=\"value\":\")([0-9\.]*)")
+record_line_id=$(echo ${record_info} | grep -Po "(?<=\"line_id\":\")([0-9\.]*)")
+echo "${SUB_DOMAIN}:${DOMAIN} record_id: ${record_id}, record_line_id: ${record_line_id}, record_value: ${record_value}"
+
+local_ip=$(curl -s ns1.dnspod.net:6666)
+echo "local ip: ${local_ip}"
+
+if [ $record_value != $local_ip ]
+then
+  echo "ip changed, will update"
+  update_res=$(curl -s -X POST https://dnsapi.cn/Record.Ddns -d "${REQUEST_PARAM}&record_id=${record_id}&record_line_id=${record_line_id}&value=${local_ip}")
+  echo $update_res
+else
+  echo "not changed, ignore"
+fi
+
